1c8a399d0d882b1f3ef60d94571fee5bc2cc8943
lisa <lisawanjiku41@gmail.com>
2025-07-27 18:31:35 +0300

    react-hooks-react-router-routes-lab-v6

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,13 +1,22 @@
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types'; 
 
-function MovieCard({title}) {
-  
+function MovieCard({ movie }) {
+  if (!movie) return null; 
   return (
     <article>
-        <h2>{title}</h2>
-        {/* What should go here? */}
+      <h2>{movie.title}</h2>
+      <Link to={`/movie/${movie.id}`}>View Info</Link>
     </article>
   );
+}
+
+//prop-typeS validation
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    id: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
+    title: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default MovieCard;
--- src/pages/Directors.jsx
+++ src/pages/Directors.jsx
@@ -1,16 +1,37 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
 
 function Directors() {
+  const [directors, setDirectors] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/directors")
+      .then((res) => res.json())
+      .then((data) => setDirectors(data))
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Director info here! */}
+        {/* Add an h1 for the page title */}
+        <h1>Directors Page</h1>
+        {directors.map((director) => (
+          <article key={director.id}>
+            <h2>{director.name}</h2>
+            <ul>
+              {director.movies.map((movie, index) => (
+                <li key={index}>{movie}</li>
+              ))}
+            </ul>
+          </article>
+        ))}
       </main>
     </>
   );
-};
+}
 
 export default Directors;
