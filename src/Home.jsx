8e8bc17b82eaf07aa8e3195dcb06c8283985e3e6
lisa <lisawanjiku41@gmail.com>
2025-07-29 14:21:50 +0300

    react-hooks-react-router-routes-lab-v6

---
+++ src/Home.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
+import MovieCard from "../components/MovieCard";
+
+function Home() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/movies")
+      .then((res) => res.json())
+      .then((data) => setMovies(data))
+      .catch((err) => console.error(err));
+  }, []);
+
+  return (
+    <>
+      <header>
+        <NavBar />
+      </header>
+      <main>
+        <h1>Home Page</h1>
+        {movies.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </main>
+    </>
+  );
+}
+
+export default Home
